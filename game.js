c16e7010d744db2261cc571833553d4a5016ed2f
Dodzi Keglo <stephanekeglo@gmail.com>
2025-01-14 23:21:57 +0000

    Implement computer choice logic

--- game.js
+++ game.js
@@ -1,4 +1,16 @@
 /**
-Rock Paper Scissors Game
-*/
+ * Rock Paper Scissors Game
+ */
 
+/**
+ * Get computer choice randomly
+ */
+function getComputerChoice () {
+  const choice = Math.floor(Math.random() * 3) + 1
+
+  if (choice === 1) {
+    return 'Rock'
+  } else if (choice === 2) {
+    return 'Paper'
+  } else return 'Scissors'
+}
