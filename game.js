122efb0ba6684751ff29c5c6d58964277bacc048
Dodzi Keglo <stephanekeglo@gmail.com>
2025-01-16 18:57:45 +0000

    Implement human choice logic

--- game.js
+++ game.js
@@ -4,13 +4,33 @@
 
 /**
  * Get computer choice randomly
+ *
+* * @returns {String}
  */
 function getComputerChoice () {
   const choice = Math.floor(Math.random() * 3) + 1
 
-  if (choice === 1) {
-    return 'Rock'
-  } else if (choice === 2) {
-    return 'Paper'
-  } else return 'Scissors'
+  if (choice === 1) return 'Rock'
+  else if (choice === 2) return 'Paper'
+  else return 'Scissors'
+}
+
+/**
+ * Get human choice
+ *
+ * @returns {String}
+ */
+function getHumanChoice () {
+  console.log(`
+  1. Rock
+  2. Paper
+  3. Scissors`)
+  let choice = ''
+  while (true) {
+    choice = Number(prompt('Enter your choice:'))
+    if (choice == 1) return 'Rock'
+    else if (choice == 2) return 'Paper'
+    else if (choice == 3) return 'Scissors'
+    else console.log('Invalid input')
+  }
 }
