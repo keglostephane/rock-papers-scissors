94814bd2de46f8e214e77f3735fb56e6bc7215d2
Dodzi Keglo <stephanekeglo@gmail.com>
2025-01-18 00:13:03 +0000

    Implement a full game and players score logic

--- game.js
+++ game.js
@@ -2,13 +2,10 @@
  * Rock Paper Scissors Game
  */
 
-let humanScore = 0
-const computerScore = 0
-
 /**
  * Get computer choice randomly
  *
-* * @returns {String}
+ * @returns {String}
  */
 function getComputerChoice () {
   const choice = Math.floor(Math.random() * 3) + 1
@@ -36,29 +33,80 @@ function getHumanChoice () {
  *
  * @param {String} humainChoice
  * @param {String} computerChoice
- *
+ * @returns {String|Null} The winner of the round otherwise `null` if no winner
  */
 function playRound (humanChoice, computerChoice) {
   if (humanChoice.toLowerCase() === 'rock') {
-    if (computerChoice === humanChoice.toLowerCase()) console.log('Draw !')
-    else if (computerChoice === 'paper') console.log(`You lost ! ${computerChoice} beats ${humanChoice}.`)
-    else {
-      console.log(`You win !' ${humanChoice} beats ${computerChoice}.`)
-      humanScore += 5
+    if (computerChoice === humanChoice.toLowerCase()) {
+		  console.log('Draw !')
+		  return null
+    } else if (computerChoice === 'paper') {
+		  console.log(`You lost ! ${computerChoice} beats ${humanChoice}.`)
+		  return 'computer'
+    } else {
+		  console.log(`You win !' ${humanChoice} beats ${computerChoice}.`)
+		  return 'human'
     }
   } else if (humanChoice.toLowerCase() === 'paper') {
-    if (computerChoice === humanChoice) console.log('Draw !')
-    else if (computerChoice === 'scissors') console.log(`You lost ! ${computerChoice} beats ${humanChoice}.`)
-    else {
-      console.log(`You win !' ${humanChoice} beats ${computerChoice}.`)
-      humanScore += 5
+    if (computerChoice === humanChoice) {
+		  console.log('Draw !')
+		  return null
+    } else if (computerChoice === 'scissors') {
+		  console.log(`You lost ! ${computerChoice} beats ${humanChoice}.`)
+		  return 'computer'
+    } else {
+		  console.log(`You win !' ${humanChoice} beats ${computerChoice}.`)
+		  return 'human'
     }
   } else if (humanChoice.toLowerCase() === 'scissors') {
-    if (computerChoice == humanChoice.toLowerCase()) console.log('Draw !')
-    else if (computerChoice === 'rock') console.log(`You lost ! ${computerChoice} beats ${humanChoice}.`)
-    else {
-      console.log(`You win !' ${humanChoice} beats ${computerChoice}.`)
-      humanScore += 5
+    if (computerChoice == humanChoice.toLowerCase()) {
+		  console.log('Draw !')
+		  return null
+    } else if (computerChoice === 'rock') {
+		  console.log(`You lost ! ${computerChoice} beats ${humanChoice}.`)
+		  return 'computer'
+    } else {
+		  console.log(`You win !' ${humanChoice} beats ${computerChoice}.`)
+		  return 'human'
     }
   }
+}
+
+/**
+ * Play a full game
+ */
+function playGame () {
+  let computerChoice
+  let humanChoice
+  let round
+  const nRounds = 5
+  let winner
+  let humanScore = 0
+  let computerScore = 0
+  const roundScore = 10
+
+  console.log('**************************************')
+  console.log('*    Rock - Paper - Scissors Game    *')
+  console.log('**************************************')
+
+  round = 1
+  while (round <= nRounds) {
+    computerChoice = getComputerChoice()
+    humanChoice = getHumanChoice()
+    console.log(`Round ${round}`)
+    winner = playRound(humanChoice, computerChoice)
+	  if (winner === 'computer') computerScore += roundScore
+	  else if (winner === 'human') humanScore += roundScore
+	  else {
+		  computerScore += roundScore / 2
+		  humanScore += roundScore / 2
+	  }
+    round++
+  }
+
+  if (computerScore > humanScore) console.log('*** Computer WIN !!! ***')
+  else if (computerScore < humanScore) console.log('*** You WIN !!! ***')
+  else console.log('*** Draw ***')
+
+  console.log(`Score - You: ${humanScore}\t Computer: ${computerScore}`)
 }
